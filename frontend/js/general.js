e5cd25072edd9a431cd9eda86e44b0b87b1de16c
Alejandro <alejandro_portaluppi@outlook.com>
2023-12-01 12:06:32 -0300

    Cambio menor

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -1,3 +1,3 @@
-const URL_BACKEND = 'http://alejandrop.pythonanywhere.com' // Esto debería ser privado, pero por ahora lo dejamos como público
+const URL_BACKEND = 'https://alejandrop.pythonanywhere.com' // Esto debería ser privado, pero por ahora lo dejamos como público
 const TOKEN_API = 'hbasdhgv'
 const PASSWORD = "123"
