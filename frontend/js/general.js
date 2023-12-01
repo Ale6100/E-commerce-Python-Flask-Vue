60bf3256dcad24bfe2d056f572316abe617ba806
Alejandro <alejandro_portaluppi@outlook.com>
2023-12-01 12:02:38 -0300

    Mejoras de estilo y accesibilidad

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -1,3 +1,3 @@
-const URL_BACKEND = 'http://127.0.0.1:5000' // Esto debería ser privado, pero por ahora lo dejamos como público
+const URL_BACKEND = 'http://alejandrop.pythonanywhere.com' // Esto debería ser privado, pero por ahora lo dejamos como público
 const TOKEN_API = 'hbasdhgv'
 const PASSWORD = "123"
